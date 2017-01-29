4b93e36627ae185cbf6e38393311542a3c40e10c
Jermaine McFarlane <j.mcfarlane.mbbs@gmail.com>
2017-01-29 14:38:11 -0500

    chore(): sync constructor and variable started

--- sync.php
+++ sync.php
@@ -2,16 +2,34 @@
 namespace VMSync;
 
 /**
- * Syncs local git changes to remote (S)FTP site
+ * Syncs local git changes to remote (S)FTP site bby finding all files changed
+ * since the last commit SHA recorded on server
  */
 class Sync
 {
+    /** current working directory */
+    public $cwd;
+    /** location of commit file */
+    public $commit_file;
+    /** files to back up */
+    public $backups;
+    /** files to ignore */
+    public $ignoredFiles;
+    /** folders to ignore */
+    public $ignoredFolders;
+    /** If true, progress of files being synced will be printed during operations */
+    public $showProgress;
 
-  function __construct()
-  {
-    # code...
-  }
+    /**
+    * Construct a new object to sync all changed files from current repository
+    * @param Site[] $sites Array of sites to sync to
+    * @param boolean $showProgress TRUE if progress bar is to be displayed
+    * @throws InvalidArgumentException if the provided argument is not of type 'array'.
+    */
+    public function __construct($sites, $showProgress = false)
+    {
+        if (!is_array($sites)) {
+            throw new InvalidArgumentException("An array of Sites must be provided");
+        }
+    }
 }
-
-
-?>
